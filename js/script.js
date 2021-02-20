d57bd9dcb6922602a9f54a9c020055c8efb619c5
Danilo <danilo.colloca91@gmail.com>
2021-02-20 13:53:11 +0100

    struttura dati contatti in vue e richiamati nella barra contatti

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
 
 new Vue({
+  el:  '#app',
 
-    el:  '',
-    data: {
-      
-     
-    },
-    methods: {
-        
+  data: {
+    
+    rotation : [
+      {
+      faIcon : 'fas fa-clock',
+      dayHour : {
+          firstDayWork : 'Mon',
+          lastDayWork : 'Sat',
+          hoursOpen : '9:00',
+          hoursClosed : '18:00'
+        }
+      }
+    ],
+    contacts : [
+      {
+        faIcon : 'fas fa-phone-alt',
+        contact : '+1(305) 1234-5678'
+      },
+      {
+        faIcon : 'fas fa-envelope',
+        contact : 'hello@example.com'
+      }
+    ],
+    social : {
+      fb : 'fab fa-facebook-f',
+      twitt : 'fab fa-twitter',
+      linked : 'fab fa-linkedin-in'
     }
+    
+  },
+  methods: {
+    //inserire trattino prima di un elemento generato in v-for che non si trovi in prima posizione
+    dashClass : function(index){ 
+      if(index !== 0){
+        return 'workdays-data-dashed'
+      }
+    }
+    
+      
+  }
   
   
-  })
+})
 
 
 
